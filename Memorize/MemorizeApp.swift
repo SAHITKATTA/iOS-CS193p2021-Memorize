83512a6b8fc7f90af997e7142735c18cee688f6d
SanjayKatta <sanjay.katta@gmail.com>
2022-02-06 01:40:49 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sanjay Katta on 06/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
